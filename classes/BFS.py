ec8b7acfe55db79870e020c68706473b16168241
steekam <waynewanyee@gmail.com>
2019-09-07 11:28:53 +0300

    updated algorithms to show optimal paths to destination

--- classes/BFS.py
+++ classes/BFS.py
@@ -7,29 +7,36 @@ class BfsTraverser:
     def __init__(self):
         self.visited = []
         self.end_search = False
+        self.paths = {}
+        self.optimal_path = []
 
     def BFS(self, graph, start_node, goal_node):
         queue = []
         queue.append(start_node)
-        # print(queue)
-        # set of visited nodes
         self.visited.append(start_node)
+        # ?Init root with start node
+        self.paths.update({start_node: [start_node]})
+
         while queue and not self.end_search:
             # Dequeue a vertex from
-            s = queue.pop(0)
-            print("Drive to", s, end="\n")
+            current_node = queue.pop(0)
 
-            # Get all adjacent vertices of the
-            # dequeued vertex s. If a adjacent
-            # has not been visited, then mark it
-            # visited and enqueue it
-            for i in list(graph[s]):
-                if i not in self.visited:
-                    if i is goal_node:
-                        self.visited.append(i)
+            for child in list(graph[current_node]):
+                if child not in self.visited:
+                    if child is goal_node:
+                        self.visited.append(child)
+                        self.optimal_path = self.paths.get(current_node) + [child]
                         self.end_search = True
                         break
                     else:
-                        queue.append(i)
+                        queue.append(child)
                         # Add node to visited
-                        self.visited.append(i)
+                        self.visited.append(child)
+                        # Add entry in paths
+                        parentPath = self.paths.get(current_node)
+                        self.paths.update({child: parentPath + [child]})
+
+        print("==== BFS Visited Nodes: ====")
+        print(self.visited, end="\n")
+        print("==== BFS Optimal Path: ====")
+        print(self.optimal_path, end="\n")
--- classes/UCS.py
+++ classes/UCS.py
@@ -5,23 +5,37 @@ class UcsTraverser:
     # Constructor
     def __init__(self):
         self.visited = []
+        self.paths = {}
+        self.least_cost_path = []
 
-    def ucs(self, graph, start_node, goal_node):
+    def UCS(self, graph, start_node, goal_node):
         queue = PriorityQueue()
         # Init with start node and cost of zero
         queue.put((0, start_node))
+        # ?Init path root
+        self.paths.update({(0, start_node): [start_node]})
 
         while queue:
             cost, current_node = queue.get()
-            print("Drive to ", current_node,
-                  "with cumulative cost => ", cost, end="\n")
+            if current_node == goal_node:
+                self.visited.append(current_node)
+                self.least_cost_path = self.paths.get((cost, current_node))
+                break
+
             if current_node not in self.visited:
                 self.visited.append(current_node)
 
-            if current_node == goal_node:
-                return
-
             for child in list(graph[current_node]):
                 if child not in self.visited:
-                    total_cost = cost + graph[current_node][child]['weight']
+                    total_cost = round(
+                        cost + graph[current_node][child]['weight'], 1)
                     queue.put((total_cost, child))
+                    # ?Add value to paths
+                    parentPath = self.paths.get((cost, current_node))
+                    self.paths.update(
+                        {(total_cost, child): parentPath + [child]})
+
+        print("==== UCS Visited Nodes: ====")
+        print(self.visited, end="\n")
+        print("==== UCS Least Cost Path: ====")
+        print(self.least_cost_path, end="\n")
