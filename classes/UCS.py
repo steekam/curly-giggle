e3f9b9d3da8bd870db9a3e9338897caf46aa595a
steekam <waynewanyee@gmail.com>
2019-09-08 22:24:16 +0300

    Change queue implementation to heapq. Added priority check for replacement of higher costs.

--- classes/UCS.py
+++ classes/UCS.py
@@ -1,4 +1,4 @@
-from queue import PriorityQueue
+from heapq import heapify, heappop, heappush
 
 
 class UcsTraverser:
@@ -7,35 +7,55 @@ class UcsTraverser:
         self.visited = []
         self.paths = {}
         self.least_cost_path = []
+        self.least_cost = 0
 
     def UCS(self, graph, start_node, goal_node):
-        queue = PriorityQueue()
+        queue = []
         # Init with start node and cost of zero
-        queue.put((0, start_node))
+        heappush(queue, (0, start_node))
         # ?Init path root
         self.paths.update({(0, start_node): [start_node]})
 
+        iteration = 1
         while queue:
-            cost, current_node = queue.get()
+            # print("Priority queue Iteration ",
+            #       iteration, [{tuple[1]: tuple[0]} for tuple in queue], end="\n")
+            # iteration += 1
+
+            cost, current_node = heappop(queue)
             if current_node == goal_node:
                 self.visited.append(current_node)
+                self.least_cost = cost
                 self.least_cost_path = self.paths.get((cost, current_node))
                 break
 
             if current_node not in self.visited:
                 self.visited.append(current_node)
 
+            # print("Current node => ", current_node, end="\n")
             for child in list(graph[current_node]):
-                if child not in self.visited:
-                    total_cost = round(
-                        cost + graph[current_node][child]['weight'], 1)
-                    queue.put((total_cost, child))
-                    # ?Add value to paths
-                    parentPath = self.paths.get((cost, current_node))
-                    self.paths.update(
-                        {(total_cost, child): parentPath + [child]})
+                # print("Child => ", child, " Visited list: ",
+                #       self.visited, end="\n")
+                # ?Calculate total cost up to current child
+                total_cost = round(
+                    cost + graph[current_node][child]['weight'], 1)
+
+                if child not in self.visited and child not in [tuple[1] for tuple in queue]:
+                    heappush(queue, (total_cost, child))
+                elif child in [tuple[1] for tuple in queue]:
+                    existing_tuple = [
+                        val for val in queue if val[1] == child][0]
+                    # ?Replace if higher cost exists in priority queue
+                    if existing_tuple[0] > total_cost:
+                        queue.remove(existing_tuple)
+                        heappush(queue, (total_cost, child))
+
+                # ?Add value to paths
+                parentPath = self.paths.get((cost, current_node))
+                self.paths.update(
+                    {(total_cost, child): parentPath + [child]})
 
         print("==== UCS Visited Nodes: ====")
         print(self.visited, end="\n")
-        print("==== UCS Least Cost Path: ====")
+        print("==== UCS Least Cost Path: (", self.least_cost, ") ====")
         print(self.least_cost_path, end="\n")
